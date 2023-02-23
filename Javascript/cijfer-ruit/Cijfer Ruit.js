9a9f16b691e8a45a7c4828ddc1787a96bea2e6ff
next-ZW <99071376@mydavinci.nl>
2023-02-23 09:06:51 +0100

    hallo

--- Javascript/cijfer-ruit/Cijfer Ruit.js
+++ Javascript/cijfer-ruit/Cijfer Ruit.js
@@ -2,26 +2,23 @@ let invoer = parseInt(prompt("Voer een getal in:"));
 let uitkomst = ""; 
 
 for (let a = 1; a <= invoer; a++){
-    for(let b = 1; b<= a; b++){
+    for(let b = 1; b <= a; b++){
         uitkomst += b.toString() + '-'
     }
-    uitkomst = uitkomst.slice(0, -1)
-    
+    uitkomst = uitkomst.slice(0,-1)
     uitkomst += "<br>"
-    console.log(uitkomst)
+
     document.getElementById("uitkomst").innerHTML += uitkomst ;
     uitkomst = ""
 }
 
-// for(let a = invoer -1; a >= 1; a--){
-//     for(let b = 1; b <= a; b++){
-//         uitkomst += b.toString() + '-'
-//     }
-//     uitkomst = uitkomst.slice(0, -1)
+for (let a = invoer -1; a >= 1; a--){
+    for(let b = 1; b <= a; b++){
+        uitkomst += b.toString() + '-'
+    }
+    uitkomst = uitkomst.slice(0,-1)
+    uitkomst += "<br>"
 
-//     uitkomst += "<br>"
-//     console.log(uitkomst)
-//     document.getElementById("uitkomst").innerHTML += uitkomst ;
-//     uitkomst = ""
-// }
-
+    document.getElementById("uitkomst").innerHTML += uitkomst ;
+    uitkomst = ""
+}
