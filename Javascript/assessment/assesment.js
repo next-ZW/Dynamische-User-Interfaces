a576dc3aec3a93fb25eff7f4276f11d38ca63355
next-ZW <99071376@mydavinci.nl>
2023-02-23 10:29:07 +0100

    assesment

---
+++ Javascript/assessment/assesment.js
@@ -0,0 +1,12 @@
+function bereken_poten(){
+    const hoeveel_giraffen = parseInt(prompt("Hoeveel Giraffen zijn er"));
+    const hoeveel_struisvogels = parseInt(prompt("Hoeveel Struisvogels zijn er"));
+    const hoeveel_zebras = parseInt(prompt("Hoeveel zebras zijn er"));
+    const Giraffen = hoeveel_giraffen * 4;
+    const Struisvogels = hoeveel_struisvogels * 2;
+    const zebras = hoeveel_zebras * 4;
+    const totaal = Giraffen + Struisvogels + zebras;
+    return totaal;
+} 
+
+console.log(bereken_poten())
