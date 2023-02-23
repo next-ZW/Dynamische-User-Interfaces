273ea9cc4b6af22636f550db7ddd01006ac8c125
next-ZW <99071376@mydavinci.nl>
2023-02-23 09:06:56 +0100

    Update horeca-app.js

--- Javascript/horeca app/horeca-app.js
+++ Javascript/horeca app/horeca-app.js
@@ -1,4 +1,18 @@
-const fris = 3
-const wijn = 7
-const bier = 4
+const fris = 4
+const bier = 3
+const wijn = 5
 
+geen_idee = " "
+
+invoer = prompt("wat wilt u bestellen")
+
+
+
+
+
+
+
+
+
+
+document.getElementById("uitkomst").innerHTML
