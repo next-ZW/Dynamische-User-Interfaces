79b0d13fc8cfa6cb198a8f99db229d348fa8f1c5
next-ZW <99071376@mydavinci.nl>
2023-03-13 11:08:48 +0100

    js

--- Javascript/horeca app/horeca-app.js
+++ Javascript/horeca app/horeca-app.js
@@ -3,24 +3,55 @@ const prijslijst = {
   bier: 1,
   wijn: 2,
 };
+const bonnetje = "";
 
 
-function bestellingen() {
-    while (true) {
-      const drankje = prompt("Wat wilt u bestellen? (fris, bier, wijn)").toLowerCase();
-      console.log(drankje)
-      if (!drankje || !prijslijst[drankje]) {
-        console.log("Sorry, dat drankje hebben we niet.");
-        continue;
-      } else (drankje === "stop") 
-      
-      const aantal = prompt(`Hoeveel ${drankje} wilt u bestellen?`);
-      if (!aantal) {
-        console.log("Bedankt voor uw bestelling!");
-        break;
+let totale_prijs = 0;
+let stop = false;
+let drinken;
+let productAmount = NaN;
+let amountNaN = true;
+
+function bestelling(){ 
+while (!stop) {
+  drinken = prompt("Wat zou u willen bestellen? kies uit fris bier wijn");
+
+  if (drinken === "stop") {
+    stop = true;
+  }
+  else {
+    if (!(drinken in dranken)) {
+      alert(`Momenteel is ${drinken} uitverkocht. Sorry voor dit ongemak.`);
+    }
+    else {
+      while (amountNaN) {
+        productAmount = parseInt(prompt(`Hoeveel ${drinken} wilt u bestellen?`));
+        if (!isNaN(productAmount)) {
+          amountNaN = false;
+        }
       }
-      Bestelling(drankje, parseInt(aantal));
+
+      amountNaN = true;
+
+      if (drinken in bonnetje) {
+        bonnetje[drinken] += productAmount;
+      }
+      else {
+        bonnetje[drinken] = productAmount;
+      }
+
+      totale_prijs += productAmount * dranken[drinken];
     }
   }
-  bestellingen();
+}
+}
 
+for (const [key, value] of Object.entries(bonnetje)) {
+  const display = `${value}x ${key} - €${(value * dranken[key]).toFixed(2)}`
+  document.getElementById("Bonnetje").innerText = "Uw Bonnetje - Bedankt voor het bestellen!";
+  document.getElementById("Dranken").innerText += display + '\n';
+}
+
+if (totale_prijs > 0) {
+  document.getElementById("Totale_prijs").innerText = "Totaal: €" + totale_prijs.toFixed(2);
+}
